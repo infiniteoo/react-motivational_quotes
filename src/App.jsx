6ea3aff0595bed72c924f149c0a172e1520d7e69
Troy Dorman <troydorman@gmail.com>
2021-05-11 08:18:49 -0600

    initialized project with CRA. integrated supplied CSS. re-created src

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                HELLO FRWENZ
+            </h1>
+        </div>
+    )
+}
+
+export default App
