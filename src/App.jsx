75b051c68d36ab3740bc058da0dd3c14fa50e5a9
Troy Dorman <troydorman@gmail.com>
2021-05-11 08:45:31 -0600

    added button to display new advice and integrated its functionality

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,16 @@ class App extends React.Component {
     render() {
         const { advice } = this.state
         return (
-            <h1>{advice}</h1>
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{advice}</h1>
+                    <button className="button" onClick={this.fetchAdvice}>
+                        <span>GIVE ME ADVICE!</span>
+
+                    </button>
+                </div>
+            </div>
+            
         )
     }
 
